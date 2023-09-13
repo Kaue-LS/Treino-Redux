7bad8976a9e3b84e254ea91e014dcc9098b7fc11
KaueLS <kaue.ls0506@gmail.com>
2023-09-12 22:16:15 -0300

    feat: add async train-3

--- src/feature/posts/AddPostForm.js
+++ src/feature/posts/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postSlice";
+import { addNewPost, postAdded } from "./postSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -10,25 +10,31 @@ const AddPostForm = () => {
     const [title, setTitle] = useState("")
     const [content, setContent] = useState('')
     const [userId, setUsersId] = useState('')
-
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
     const users = useSelector(selectAllUsers)
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUsersId(e.target.value)
 
-    const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title, content, userId)
-            )
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
-            setTitle('')
-            setContent('')
+    const onSavePostClicked = () => {
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
+                setTitle('')
+                setContent('')
+                setUsersId('')
+            } catch (err) {
+                console.log('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     }
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
     const usersOptions = users.map(user => (
         <option key={user.id} value={user.id}>
--- src/feature/posts/postList.js
+++ src/feature/posts/postList.js
@@ -1,32 +1,37 @@
-import { useSelector } from "react-redux/es/hooks/useSelector";
-import { selectAllPost } from "./postSlice";
-import PostAuthor from "./postAuthor";
-import { TimeAgo } from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux/";
+import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./postSlice";
+import PostsExcerpt from "./PostsExcept";
 
 const PostList = () => {
+    const dispatch = useDispatch();
 
-    const posts = useSelector(selectAllPost)
+    const posts = useSelector(selectAllPosts)
+    const postsStatus = useSelector(getPostsStatus)
+    const error = useSelector(getPostsError)
+
+    useEffect(() => {
+        if (postsStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postsStatus, dispatch])
+
+    let content;
+    if (postsStatus === 'loading') {
+        content = <p>"Loading..."</p>;
+
+    } else if (postsStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+
+    } else if (postsStatus === 'failed') {
+        content = <p>{error}</p>
+    }
     console.log(posts)
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p>
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
-
     return (
         <section>
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
